c90017bfee5a8d9111a157f6b7cce8c998c5a2ed
Surabhi Khachar <surabhikhachar@ku.edu>
2018-10-06 13:27:36 -0500

    Pushing everythinggit add .git add .

---
+++ EECS_731_Project_3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project involving MovieLens datasets. It combines feature engineering and basic clustering principles to build a sort of movie recommendation system using ratings, tags, and other features. ',
+    author='Surabhi Khachar',
+    license='',
+)
